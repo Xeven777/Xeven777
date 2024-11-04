f6fe51bed056585c773612ab91a3934cbb1f45b0
Anish <xevenbiswas@gmail.com>
2024-11-05 01:42:52 +0530

    Update getNewBento.ts

--- getNewBento.ts
+++ getNewBento.ts
@@ -1,5 +1,4 @@
-const apiUrl =
-  "https://opbento.edgexhq.tech/api/bento?n=Anish%20Biswas&g=xeven777&x=xevebiswas&l=anishbiswas777&i=https%3A%2F%2Fblastro.netlify.app%2Fassets%2Fanish.webp&p=anish7.me&z=a3a2f";
+const apiUrl = "https://opbento.edgexhq.tech/api/bento?n=Anish%20Biswas&g=xeven777&x=xevenbiswas&l=anishbiswas777&i=https%3A%2F%2Fblastro.netlify.app%2Fassets%2Fanish.webp&p=anish7.me&z=dea58";
 interface BentoResponse {
   url: string;
 }
